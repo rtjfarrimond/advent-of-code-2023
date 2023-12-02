751a781384f24ae2e9ab2a4407ebc6af8c066ec5
Robert Farrimond <robert.farrimond@zego.com>
2023-12-02 13:07:22 +0000

    D01P2

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -1,18 +1,54 @@
+import scala.annotation.tailrec
+
 @main def run(): Unit =
-  println(s"Part1: ${Day01.part1()}")
+  val input = InputLoader.load("day01.txt").toList
+  println(s"Part1: ${Day01.part1(input)}")
+  println(s"Part2: ${Day01.part2(input)}")
 
 object Day01 {
 
-  private val input = InputLoader.load("day01.txt")
+  private val numberChars = '0' to '9'
 
-  def part1(): Int = {
-    val numberChars = ('0' to '9').toSet
-    val ints: List[Int] = input.map { str =>
+  def part1(input: List[String]): Int =
+    lineToInt(input).sum
+
+  def part2(input: List[String]): Int =
+    lineToInt(input.map(parseNumbers)).sum
+
+  private def lineToInt(input: List[String]): List[Int] =
+    input.map { str =>
       val filtered = str.filter(numberChars.contains)
       s"${filtered.head}${filtered.last}".toInt
-    }.toList
+    }
 
-    ints.sum
+  def parseNumbers(input: String): String = {
+    val numberNames = Map(
+      "zero" -> 0,
+      "one" -> 1,
+      "two" -> 2,
+      "three" -> 3,
+      "four" -> 4,
+      "five" -> 5,
+      "six" -> 6,
+      "seven" -> 7,
+      "eight" -> 8,
+      "nine" -> 9
+    )
+
+    @tailrec
+    def loop(left: String, right: String): String =
+      right.headOption match
+        case None => left
+        case Some(next) =>
+          val maybeNumberMapping = numberNames.find { case (numberName, _) =>
+            right.startsWith(numberName)
+          }
+          val nextLeft = maybeNumberMapping match
+            case Some((_, number)) => s"$left$number"
+            case None => s"$left$next"
+          loop(nextLeft, right.tail)
+
+    loop("", input).filter(numberChars.contains)
   }
 
 }
--- src/test/scala/Day01Test.scala
+++ src/test/scala/Day01Test.scala
@@ -1,7 +1,59 @@
 class Day01Test extends munit.FunSuite {
   test("part1") {
-    val obtained = Day01.part1()
+    val input = List(
+      "1abc2",
+      "pqr3stu8vwx",
+      "a1b2c3d4e5f",
+      "treb7uchet"
+    )
+    val obtained = Day01.part1(input)
     val expected = 142
     assertEquals(obtained, expected)
   }
+
+  test("part2") {
+    val input = List(
+      "two1nine",
+      "eightwothree",
+      "abcone2threexyz",
+      "xtwone3four",
+      "4nineeightseven2",
+      "zoneight234",
+      "7pqrstsixteen"
+    )
+    val obtained = Day01.part2(input)
+    val expected = 281
+    assertEquals(obtained, expected)
+  }
+
+  test("parseNumbers") {
+    val input = List(
+      "two1nine",
+      "eightwothree",
+      "abcone2threexyz",
+      "xtwone3four",
+      "4nineeightseven2",
+      "zoneight234",
+      "7pqrstsixteen"
+    )
+    val expected =  List(
+      "219",
+      "823",
+      "123",
+      "2134",
+      "49872",
+      "18234",
+      "76"
+    )
+    val actual = input.map(Day01.parseNumbers)
+    assertEquals(actual, expected)
+  }
+
+  test("parseNumbers should parse even when numbers overlap") {
+    val input = "nineightwone"
+    val expected = "9821"
+    val actual = Day01.parseNumbers(input)
+    assertEquals(actual, expected)
+  }
+
 }
